f1ad8338da3aacb7d01eb6ec14696b75cb6243d8
LeonardoFrazzetto <leonardo.frazzetto1990@gmail.com>
2021-07-05 18:21:38 +0200

    Renombrando carpetas train/test

--- src/utils/mining_data_tb.py
+++ src/utils/mining_data_tb.py
@@ -4,10 +4,8 @@ import os
 
 dir = os.path.dirname
 sep = os.sep
-ml_project_path = dir(dir(dir(os.path.abspath(__file__))))
-sys.path.append(ml_project_path)
-
-
+project_path = dir(dir(dir(os.path.abspath(__file__))))
+sys.path.append(project_path)
 
 def nombres_carpetas_ok(lista_carpetas):
     lista_carpetas_ok = []
@@ -20,8 +18,11 @@ def nombres_carpetas_ok(lista_carpetas):
     return lista_carpetas_ok
 
 
-def renombrar_carpetas(lista_nombres_ok):
-    lista_original_carpetas = os.listdir('data/fotos_perros/train')
-    for i in lista_nombres_ok:
-        for j in lista_original_carpetas:
+def renombrar_carpetas(set_to_rename):
+    for carpeta in os.listdir(project_path + sep + 'data' + sep + 'fotos_perros' + sep + set_to_rename):
+        nueva_carpeta = carpeta[10::]
+        nueva_carpeta = nueva_carpeta.replace('-', ' ')
+        nueva_carpeta = nueva_carpeta.replace('_', ' ')
+        nueva_carpeta = nueva_carpeta.title()
+        os.rename(project_path + sep + 'data' + sep + 'fotos_perros' + sep + set_to_rename + sep + carpeta, project_path + sep + 'data' + sep + 'fotos_perros' + sep + set_to_rename + sep + nueva_carpeta)
             
